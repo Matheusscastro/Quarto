2ac919d596b608691c525553a1b7a13b7487feb2
Matheusscastro <116644492+Matheusscastro@users.noreply.github.com>
2023-05-19 17:57:05 -0300

    Add files via upload

---
+++ Quarto.java
@@ -0,0 +1,41 @@
+package MatheusCastro;
+
+public class Quarto {
+
+	private Long idQuarto;
+	private String descricao;
+	private int quantVagas;
+	private Long idFoto;
+	
+	
+	public Long getIdQuarto() {
+		return idQuarto;
+	}
+	public void setIdQuarto(Long idQuarto) {
+		this.idQuarto = idQuarto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getQuantVagas() {
+		return quantVagas;
+	}
+	public void setQuantVagas(int quantVagas) {
+		this.quantVagas = quantVagas;
+	}
+	
+	public void incluiFoto() {
+		
+	}
+	
+	public void listarFoto(Fotos idFoto) {
+		
+	}
+	
+	public void deletarFoto(Fotos idFoto) {
+		
+	}
+}
